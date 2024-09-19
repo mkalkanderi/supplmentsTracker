bf8267c0c8b21c3c4540f90744137e07f5c5ed58
Mohammed Alkandari <mohammedalkandari8@gmail.com>
2024-09-19 08:29:11 +0300

    initial commit
    initial commit

---
+++ supplments-viewer/supplments-viewer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  supplments-viewer
+//
+//  Created by Mohammad Alkandari on 19/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ supplments-viewer/supplments-viewer/supplments_viewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  supplments_viewerApp.swift
+//  supplments-viewer
+//
+//  Created by Mohammad Alkandari on 19/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct supplments_viewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
